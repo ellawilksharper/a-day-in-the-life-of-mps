527512668928458314dc3edad4a6e7984829675b
Ella Wilks-Harper <ellawilksharper@hotmail.co.uk>
2017-04-15 19:51:44 +0100

    fix syntax error

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,26 @@
 MAPPING = {
     'theresamay': [
         {
-            'text': 'tmay tweet'
-            'username': 'tmay_tweeter'
-            'date': 'April 13, 2017'
+            'text': 'tmay tweet',
+            'username': 'tmay_tweeter',
+            'date': 'April 13, 2017',
         },
         {
-            'text': 'tmay tweet2'
-            'username': 'tmay_tweeter2'
-            'date': 'April 15, 2017'
+            'text': 'tmay tweet2',
+            'username': 'tmay_tweeter2',
+            'date': 'April 15, 2017',
         }
     ]
     'jeremycorbyn': [
         {
-            'text': 'jcorbyn tweet'
-            'username': 'jcorbyn_tweeter'
-            'date': 'April 14, 2017'
+            'text': 'jcorbyn tweet',
+            'username': 'jcorbyn_tweeter',
+            'date': 'April 14, 2017',
         },
         {
-            'text': 'jcorbyn tweet2'
-            'username': 'jcorbyn_tweeter2'
-            'date': 'April 15, 2017'
+            'text': 'jcorbyn tweet2',
+            'username': 'jcorbyn_tweeter2',
+            'date': 'April 15, 2017',
         }
     ]
 }
